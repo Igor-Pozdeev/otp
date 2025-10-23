ae87b3fbc7232565a440abc9dcb32ebbc49c2b88
Igor <Sketch_90@mail.ru>
2025-10-23 20:26:32 +0300

    HW-18: Исправление замечаний по single responsibility

--- src/main/java/ru/pozdeev/otp/service/KafkaResponseService.java
+++
@@ -1,8 +0,0 @@
-package ru.pozdeev.otp.service;
-
-import ru.pozdeev.otp.dto.kafka.KafkaResponse;
-
-public interface KafkaResponseService {
-
-    void processKafkaResponse(KafkaResponse kafkaResponse);
-}
